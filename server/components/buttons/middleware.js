03a146464cef2b72c2361d3724858afb2850edda
erick <erickgarcia@paypal.com>
2021-08-19 15:36:03 -0500

    Remove api call to merchantID when resolving personalization promise

--- server/components/buttons/middleware.js
+++ server/components/buttons/middleware.js
@@ -111,11 +111,10 @@ export function getButtonMiddleware({
 
             const personalizationEnabled = getPersonalizationEnabled(req);
             const personalizationPromise = promiseTimeout(
-                merchantIDPromise.then(merchantID =>
-                    resolvePersonalization(req, gqlBatch, {
-                        logger, clientID, merchantID, buyerCountry, locale, buttonSessionID, currency, intent, commit,
-                        vault, label, period, tagline, personalizationEnabled, renderedButtons
-                    })),
+                resolvePersonalization(req, gqlBatch, {
+                    logger, clientID,  buyerCountry, locale, buttonSessionID, currency, intent, commit,
+                    vault, label, period, tagline, personalizationEnabled, renderedButtons
+                }),
                 EXPERIMENT_TIMEOUT
             ).catch(() => {
                 return {};
